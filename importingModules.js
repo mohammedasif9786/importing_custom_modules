e1a2c0bb74bbf3607bc7beb40d8e869e750a8100
mohammedasif9786 <asif.jdp@gmail.com>
2024-02-04 22:11:23 +0530

    imprting custom modules in node js

---
+++ importingModules.js
@@ -0,0 +1,11 @@
+
+let math = require('./module')
+let {add,multiply,divide,power,substract}=math
+
+console.log(add(8,9))
+console.log(multiply(2,7))
+console.log(divide(134,9))
+console.log(substract(13,9))
+console.log(power(8,9))
+
+
